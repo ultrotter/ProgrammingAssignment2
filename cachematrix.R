e59c28558ae311146ee70237c2136821b0539bfc
Guido Trotter <ultrotter@gmail.com>
2015-12-16 11:49:30 +0100

    Implement trivial makeCacheMatrix and cacheSolve
    Code is based on the cachevector.R example

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R is a solution to "R Programming" assignment 2.
+##
+## makeCacheMatrix allows creating an object holding a matrix and optionally a
+## cache to its solution.
 
-## Write a short comment describing this function
+## cacheSolve will use the object to return the solution, using the cache.
 
-# TODO: all work :)
-
+## Create a "CacheMatrix" object, which contains
+##   - a matrix
+##   - a cached solution
+##   - getter and setter methods for the solution
+##   - getter and setter methods for the matrix
+##
+## Note: setting a new matrix invalidates the cached solution
 makeCacheMatrix <- function(x = matrix()) {
+        # Cached solution
+        solution <- NULL
 
+        # Matrix setter, update matrix and invalidate solution
+        set <- function(y) {
+                x <<- y
+                solution <<- NULL
+        }
+
+        # Read the matrix
+        get <- function() x
+
+        # Cache the solution
+        setsol <- function(sol) solution <<- sol
+
+        # Return cached solution
+        getsol <- function() solution
+
+        # List of methods, returned
+        list(set = set, get = get,
+             setsol = setsol,
+             getsol = getsol)
 }
 
 
-## Write a short comment describing this function
-
+## Given a "CacheMatrix" return the solution
+##   - if the solution is in the cache, it is returned
+##   - if the cache is empty the solution is also cached for future use
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        sol <- x$getsol()
+        if(!is.null(sol)) {
+                message("using cached data")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setsol(sol)
+        sol
 }
